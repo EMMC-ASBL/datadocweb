20815cdaf4c11f4102c5b8967a3bcd0ca9854cd4
Treesa Joseph <66374813+Treesarj@users.noreply.github.com>
2025-08-15 15:24:34 +0200

    download template feature

--- datadoc/urls.py
+++ datadoc/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("explore/", views.explore, name="explore"),
     path("upload/", views.upload, name="upload"),
     path("editForm/", views.editForm, name="editForm"),
+    path("download/<str:filename>/", views.download_template, name="download_template"),
 ]
--- datadoc/views.py
+++ datadoc/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import FileResponse, Http404
+import os
+import mimetypes
+from django.conf import settings
 
 
 def index(request):
@@ -20,3 +23,16 @@ def upload(request):
 
 def explore(request):
     return render(request, "datadoc/views/explore.html")
+
+
+def download_template(request, filename):
+    template_path = os.path.join(
+        settings.BASE_DIR, "core/static/core/templates", filename
+    )
+    if os.path.exists(template_path):
+        mime_type, _ = mimetypes.guess_type(template_path)
+        response = FileResponse(open(template_path, "rb"), content_type=mime_type)
+        response["Content-Disposition"] = f'attachment; filename="{filename}"'
+        return response
+    else:
+        raise Http404("Template not found")
