de364cc3da570b5c01b2e055114f9f318fda210b
Treesa Joseph <66374813+Treesarj@users.noreply.github.com>
2025-08-18 15:38:02 +0200

    update file upload for csv and excel

--- datadoc/urls.py
+++ datadoc/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("home/", views.explore, name="home"),
     path("explore/", views.explore, name="explore"),
     path("upload/", views.upload, name="upload"),
+    path("upload/file/", views.upload_file, name="upload_file"),
     path("edit-form/", views.edit_form, name="edit_form"),
     path("download/<str:filename>/", views.download_template, name="download_template"),
 ]
--- datadoc/views.py
+++ datadoc/views.py
@@ -3,6 +3,15 @@ from django.http import FileResponse, Http404
 import os
 import mimetypes
 from django.conf import settings
+import pandas as pd
+from tripper.datadoc import TableDoc
+from tripper import Triplestore
+import tempfile
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+
+select_iri = "http://localhost:7200/repositories/matchmaker"
+update_iri = "http://localhost:7200/repositories/matchmaker/statements"
 
 
 def index(request):
@@ -36,3 +45,26 @@ def download_template(request, filename):
         return response
     else:
         raise Http404("Template not found")
+
+
+@csrf_exempt  # Remove this if CSRF is configured properly and handled in your template
+def upload_file(request):
+    if request.method == "POST" and request.FILES.get("files"):
+        uploaded_file = request.FILES["files"]
+
+        ts = Triplestore("sparqlwrapper", base_iri=select_iri, update_iri=update_iri)
+
+        if uploaded_file.name.endswith((".xls", ".xlsx", ".csv")):
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".csv") as temp_file:
+                for chunk in uploaded_file.chunks():
+                    temp_file.write(chunk)
+                temp_file_path = temp_file.name
+            try:
+                td = TableDoc.parse_csv(temp_file_path)
+                td.save(ts)
+                result_message = f"{uploaded_file.name} has populated the Graph"
+            except Exception as e:
+                result_message = f"Error: {str(e)}"
+            finally:
+                os.remove(temp_file_path)
+        return JsonResponse({"status": "success", "message": result_message})
